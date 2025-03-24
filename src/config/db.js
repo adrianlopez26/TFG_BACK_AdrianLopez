3356800fe30fba03ec7cf4bc6ec0918d3a2f458c
adrian.lopez <adrian.lopezm@campusfp.es>
2025-03-24 16:27:52 +0100

    Modify T.users 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a MySQL:', err);
-        return;
-    }
-    console.log('🟢 Conexión a MySQL exitosa');
-});
-
-module.exports = connection;
+module.exports = pool;
